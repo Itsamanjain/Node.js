0cb83615c93c4e7f1398ff32d058bddd720a8d6d
Itsamanjain <112370476+Itsamanjain@users.noreply.github.com>
2023-08-02 18:29:01 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+// const math = require('./math.js');
+const {add,sub} = 
+
+console.log("hello world");
+
+console.log("math value is ",math.add(1, 2));
+console.log("math value is ",math.sub(1, 2));
