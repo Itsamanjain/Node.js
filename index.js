dc9a7931b6ec97a1305aba18ef59a61b5af4ad24
Itsamanjain <112370476+Itsamanjain@users.noreply.github.com>
2023-08-03 11:20:01 +0530

    node

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require( 'http' );
+const fs = require( 'fs' );
+
+const myServer = http.createServer( (request, response) => {
+    const log = `${Date.now()}: ${request.url} New Request Recived\n`;
+    fs.appendFile('log.txt',log, (err,data) => {
+        // switch(request.url){
+        //     case "/":
+        //         response.end("Homepage")
+        //         break;
+        //     case "/about":
+        //         response.end("This is about page");
+        //         break;
+
+        //     default:
+        //         response.end("404 not found");
+        // }
+        response.end("Hello from server");
+    })
+    // console.log("New Request Received");
+    console.log(request);
+    
+}); 
+
+myServer.listen(8000,() => console.log("server started !"));
